ed258aefc810976abbc5edea3be6e1d3b0579995
Alexander <aseldom@yandex.ru>
2023-03-06 17:51:31 +0600

    3. Каркас шахматной доски [#792 #397911]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.chess;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.black.BishopBlack;
@@ -10,8 +9,6 @@ import ru.job4j.chess.firuges.white.KingWhite;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
-@Disabled("Тесты отключены. Удалить аннотацию после реализации всех методов по заданию.")
 public class LogicTest {
 
     @Test
@@ -33,12 +30,12 @@ public class LogicTest {
         ImpossibleMoveException exception = assertThrows(ImpossibleMoveException.class, () -> {
             logic.move(Cell.C1, Cell.C2);
         });
-        assertThat(exception.getMessage()).isEqualTo(String.format
-                ("Could not move by diagonal from %s to %s", Cell.C1, Cell.C2));
+        assertThat(exception.getMessage()).isEqualTo(String.format(
+                "Could not move by diagonal from %s to %s", Cell.C1, Cell.C2));
     }
 
     @Test
-    public void whenMoveThenOccupiedCellException ()
+    public void whenMoveThenOccupiedCellException()
             throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
         KingWhite kingWhite = new KingWhite(Cell.C1);
         KingBlack kingBlack = new KingBlack(Cell.H6);
@@ -48,23 +45,9 @@ public class LogicTest {
         OccupiedCellException exception = assertThrows(OccupiedCellException.class, () -> {
             logic.move(Cell.C1, Cell.H6);
         });
-        assertThat(exception.getMessage()).isEqualTo(String.format
-                ("Cell %s occupied", Cell.H6));
+        assertThat(exception.getMessage()).isEqualTo(String.format(
+                "Cell %s occupied", Cell.H6));
     }
-
-
-
-//    @Test
-//    public void moveWhenBishopBlackC1ThenH6()
-//            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-//        Logic logic = new Logic();
-//        Cell source = Cell.C1;
-//        Cell dest = Cell.H6;
-//        BishopBlack bishopBlack = new BishopBlack(source);
-//        logic.add(bishopBlack);
-//        logic.move(source, dest);
-//        assertThat(logic.).isEqualTo(dest);
-//    }
 }
 
 
